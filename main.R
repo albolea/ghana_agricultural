1bcaa5932f62f9ee4710cf9036cf8f223239b550
Renato Albolea <albolea@gmail.com>
2018-11-30 13:45:59 -0800

    inclusion of variable
    Landa size, Education, Sex, Have Spouse, do_math

--- main.R
+++ main.R
@@ -8,7 +8,7 @@ library(haven)
 #Load Farm Profit
 profit_hh <- read_dta(here("raw_data/glss4/aggregates","agg2.dta"))
 #exclude depreciation from profit and exclude profit = 0
-profit_hh <- profit_hh %>% mutate(profit= agri1-hhagdepn) %>% 
+profit_hh <- profit_hh %>% mutate(profit= agri1c-hhagdepn) %>% 
     filter(profit > 0 | profit < 0 ) %>% 
     mutate(ID = paste(clust,nh,sep="_")) %>% 
     select(ID,profit)
@@ -52,19 +52,22 @@ land_size <- land_size %>% rename(farm_size = s8bq4a,unit = s8bq4b) %>%
 
 #transfor mesuare to Acres, 1 Pole = 1 Acre and 1 Rope = 1/9Acre and sum the farm sizes
 land_size <- land_size %>% 
-  mutate(farm_size = ifelse( unit == 3,farm_size/9,
-                             ifelse( unit == 4, 0,farm_size))
-  ) %>% 
+  filter(unit<4) %>% #excluding other measures
+  mutate(farm_size = ifelse( unit == 3,farm_size/9,farm_size)) %>% 
+  filter(farm_size > 0 & farm_size < 1e+100) %>% 
   select(ID,farm_size) %>% 
   group_by(ID) %>% 
   summarise(farm_size = sum(farm_size))
+
 
 
 inf_farmers <- read_dta(here("raw_data/glss4","sec6.dta")) #Identification of farm and No-n farm Enterprises
 
 inf_farmers <- inf_farmers %>% filter(s6q1==1) #exclude non farmers
 inf_farmers <- inf_farmers %>% rename(farmer = s6q1, fishing = s6q4, own_business = s6q8) %>% 
-  mutate(ID = paste(clust,nh,sep="_")) %>% select(ID,farmer,fishing,own_business)
+  mutate(ID = paste(clust,nh,sep="_"),
+         fishing = fishing==1,
+         own_business = own_business==1) %>% select(ID,farmer,fishing,own_business)
 
 
 
@@ -86,3 +89,68 @@ base %>% filter(fishing==2 & own_business==2) %>%  group_by(loc5) %>%
 
 base %>% group_by(loc5) %>% 
   ggplot(aes(x=loc5,y=profit_per_acre_usd,group=loc5)) +geom_boxplot()
+
+
+base %>%ggplot(aes(x=profit_per_acre_usd)) +geom_density()
+
+base %>% select(farm_size) %>% summary()
+
+base %>% filter(farm_size<50) %>%  ggplot(aes(farm_size,profit)) +geom_point()
+base %>% select(profit_per_acre,farm_size) %>% summary()
+
+
+family_info <- read_dta(here("raw_data/glss4","sec1.dta")) 
+family_info <- family_info %>% rename(spouse_live_hh = s1q7) %>% 
+  mutate(ID = paste(clust,nh,sep="_"),
+         spouse_live_hh = spouse_live_hh==1,
+         sex_male = sex==1) %>% 
+  select(ID,spouse_live_hh,sex_male,agey)
+
+base <- base %>% left_join(family_info)
+
+educ_info <- read_dta(here("raw_data/glss4","sec2a.dta")) 
+educ_info <- educ_info %>% select(clust,nh,pid,s2aq1,s2aq3) %>% 
+  mutate(ID = paste(clust,nh,sep="_"),
+         educ_none = s2aq1==2 | s2aq3==1,
+         educ_bece = s2aq1==1 & s2aq3==2,
+         educ_voc = s2aq1==1 & s2aq3==3,
+         educ_O_level = s2aq1==1 & s2aq3==4,
+         educ_sss = s2aq1==1 & s2aq3==5,
+         educ_a_level = s2aq1==1 & s2aq3==6,
+         educ_t_cert = s2aq1==1 & (s2aq3==7 |s2aq3==8),
+         educ_nurse = s2aq1==1 & s2aq3==9,
+         educ_tech = s2aq1==1 & (s2aq3==10 |s2aq3==11),
+         educ_bach = s2aq1==1 & s2aq3==12,
+         educ_master = s2aq1==1 & s2aq3==13,
+         educ_phd = s2aq1==1 & s2aq3==14,
+         educ_other = s2aq1==1 & s2aq3==96
+  ) %>% 
+  select(-s2aq1,-s2aq3,-clust,-nh)
+
+base <- base %>% left_join(educ_info)
+
+mod_2 <- lm(profit_per_acre~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
+              educ_bece+educ_voc +educ_O_level +educ_sss +educ_a_level +educ_t_cert +
+              educ_nurse +educ_tech +educ_bach +educ_master +educ_phd +educ_other,data=base)
+summary(mod_2)
+
+base_no_fishing <- base %>% filter(fishing==FALSE)
+mod_2 <- lm(profit_per_acre~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
+              educ_none,data=base_no_fishing)
+summary(mod_2)
+
+mod_3 <- lm(profit_per_acre~farm_size+spouse_live_hh+sex_male+fishing+
+              educ_none,data=base)
+summary(mod_3)
+
+
+educ_info_2 <- read_dta(here("raw_data/glss4","sec2c.dta")) 
+educ_info_2 <- educ_info_2 %>% 
+  mutate(ID = paste(clust,nh,sep="_"),
+         do_math = s2cq5==1) %>% 
+  select(ID,pid,do_math)
+
+base <- base %>% left_join(educ_info_2)
+mod_4 <- lm(profit_per_acre~farm_size+spouse_live_hh+sex_male+fishing+
+              educ_none+do_math,data=base)
+summary(mod_4)
