69c4cf4afcab4a3f0760e6ab7b3f16c3a53dcdbb
Renato Albolea <albolea@gmail.com>
2018-12-04 16:42:42 -0800

    Inclusion of Community survey Log Transformation Exclusion of Urban areas

--- main.R
+++ main.R
@@ -3,12 +3,13 @@ library(here)
 library(GGally)
 library(car)
 library(haven)
+library(fastDummies)
 
 ###### Load and treat files #####
 #Load Farm Profit
 profit_hh <- read_dta(here("raw_data/glss4/aggregates","agg2.dta"))
 #exclude depreciation from profit and exclude profit = 0
-profit_hh <- profit_hh %>% mutate(profit= agri1c-hhagdepn) %>% 
+profit_hh <- profit_hh %>% mutate(profit= agri1c) %>% #-hhagdepn
     filter(profit > 0 | profit < 0 ) %>% 
     mutate(ID = paste(clust,nh,sep="_")) %>% 
     select(ID,profit)
@@ -20,12 +21,12 @@ summary(profit_hh)
 inf_house <- read_dta(here("raw_data/glss4","sec0a.dta"))  #DATA ABOUT HOUSE HOLD
 
 #Name Tables
-region_name <- c("Western","Central","Greater Accra","Eastern","Volta","Ashanti",
-                 "Brong Ahafo","Northern","Upper East","Upper West")
+region_name <- c("Western","Central","Greater_Accra","Eastern","Volta","Ashanti",
+                 "Brong_Ahafo","Northern","Upper_East","Upper_West")
 eco_zone_name <- c("Costal","Forest", "Savannah")
 loc2_name <- c("Urban","Rural")
-loc5_name <- c("Accra","Other Urban","Rural Coastal","Rural Forest","Rural Savannah")
-loc3_name <- c("Accra","Other Urban","Rural")
+loc5_name <- c("Accra","Other_Urban","Rural_Coastal","Rural_Forest","Rural_Savannah")
+loc3_name <- c("Accra","Other_Urban","Rural")
 
 
 #Treating information about house hold
@@ -43,8 +44,7 @@ inf_house <- inf_house %>% mutate(ID = paste(clust,nh,sep="_"),
                                                                      loc2,
                                                                      loc5,
                                                                      loc3) 
-
-
+inf_house <- inf_house %>% dummy_cols(select_columns =c("region","ez","loc2"))
 ### LAND SIZE
 land_size <- read_dta(here("raw_data/glss4","sec8b.dta")) #Second source of land size
 land_size <- land_size %>% rename(farm_size = s8bq4a,unit = s8bq4b) %>%
@@ -66,23 +66,43 @@ inf_farmers <- read_dta(here("raw_data/glss4","sec6.dta")) #Identification of fa
 inf_farmers <- inf_farmers %>% filter(s6q1==1) #exclude non farmers
 inf_farmers <- inf_farmers %>% rename(farmer = s6q1, fishing = s6q4, own_business = s6q8) %>% 
   mutate(ID = paste(clust,nh,sep="_"),
-         fishing = fishing==1,
-         own_business = own_business==1) %>% select(ID,farmer,fishing,own_business)
+         fishing = fishing==1,## Dummy
+         own_business = own_business ==1) %>%  ##Dummy
+  select(ID,farmer,fishing,own_business)
 
 
 
 ###### Join in one variable
-
 base <- inf_house %>% 
   left_join(inf_farmers, by = "ID") %>% 
   left_join(land_size, by="ID") %>% 
   left_join(profit_hh,by="ID")
 
 
-base <- base %>% filter(!is.na(farm_size)&!is.na(profit))
+base <- base %>% filter(!is.na(farm_size) & !is.na(profit) & loc2 == "Rural")
+#base <- base %>% filter(!is.na(farm_size) & !is.na(profit) )
 base <- base %>% mutate(profit_per_acre = profit/farm_size,
                         profit_usd = profit/3550,
                         profit_per_acre_usd = profit_usd/farm_size)
+
+#scale profit to use log
+base <- base %>%
+  mutate(
+    profit_log = log(profit-(min(profit)-10000)),
+    profit_per_acre_log = log((profit-(min(profit)-10000))/farm_size)
+  )
+
+#exclude outliers 99% confidence
+se = sd(base$profit_per_acre)
+avg = mean(base$profit_per_acre)
+base <- base %>% filter(profit_per_acre>(avg-2.326*se)&profit_per_acre<(avg+2.326*se))
+
+# #scale profit to use log
+# base <- base %>%
+#   mutate(
+#     profit_log = log(profit-(min(profit)-10000)),
+#     profit_per_acre_log = log((profit-(min(profit)-10000))/farm_size)
+#   )
 
 base %>% filter(fishing==2 & own_business==2) %>%  group_by(loc5) %>% 
   ggplot(aes(x=loc5,y=profit_per_acre_usd,group=loc5)) +geom_boxplot()
@@ -93,10 +113,13 @@ base %>% group_by(loc5) %>%
 
 base %>%ggplot(aes(x=profit_per_acre_usd)) +geom_density()
 
+base %>%ggplot(aes(x=profit_per_acre_usd)) +geom_histogram()
+
 base %>% select(farm_size) %>% summary()
 
-base %>% filter(farm_size<50) %>%  ggplot(aes(farm_size,profit)) +geom_point()
-base %>% select(profit_per_acre,farm_size) %>% summary()
+base %>% filter(farm_size<50,profit_per_acre_usd<2500) %>%  
+  ggplot(aes(farm_size,profit_per_acre_usd)) +geom_point()
+base %>% select(profit_per_acre_log,farm_size) %>% summary()
 
 
 family_info <- read_dta(here("raw_data/glss4","sec1.dta")) 
@@ -124,21 +147,57 @@ educ_info <- educ_info %>% select(clust,nh,pid,s2aq1,s2aq3) %>%
          educ_bach = s2aq1==1 & s2aq3==12,
          educ_master = s2aq1==1 & s2aq3==13,
          educ_phd = s2aq1==1 & s2aq3==14,
-         educ_other = s2aq1==1 & s2aq3==96
+         educ_other = s2aq1==1 & s2aq3==96,
+         educ_advanced = s2aq1==1 & s2aq3>2
   ) %>% 
   select(-s2aq1,-s2aq3,-clust,-nh)
 
 base <- base %>% left_join(educ_info)
+
+# mod_1 <- lm(profit_per_acre~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
+#               educ_bece+educ_voc +educ_O_level +educ_sss +educ_a_level +educ_t_cert +
+#               educ_nurse +educ_tech +educ_bach +educ_master +educ_phd +educ_other+
+#               region_Western+region_Central+region_Greater_Accra+
+#               region_Eastern+region_Volta+region_Ashanti+region_Brong_Ahafo+
+#               region_Northern+region_Upper_East,data=base)
+# summary(mod_1)
+
+mod_1 <- lm(profit_per_acre~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
+              educ_bece+educ_advanced+
+              region_Western+region_Central+region_Greater_Accra+
+              region_Eastern+region_Volta+region_Ashanti+region_Brong_Ahafo+
+              region_Northern+region_Upper_East,data=base)
+summary(mod_1)
+mod_1_log <- lm(profit_per_acre_log~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
+              educ_bece+educ_advanced+
+              region_Western+region_Central+region_Greater_Accra+
+              region_Eastern+region_Volta+region_Ashanti+region_Brong_Ahafo+
+              region_Northern+region_Upper_East,data=base)
+summary(mod_1_log)
+
+mod_1b <- lm(profit_per_acre~farm_size+spouse_live_hh+fishing+ez_Costal+ez_Forest,data=base)
+summary(mod_1b)
+
+    
 
 mod_2 <- lm(profit_per_acre~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
               educ_bece+educ_voc +educ_O_level +educ_sss +educ_a_level +educ_t_cert +
               educ_nurse +educ_tech +educ_bach +educ_master +educ_phd +educ_other,data=base)
 summary(mod_2)
 
-base_no_fishing <- base %>% filter(fishing==FALSE)
-mod_2 <- lm(profit_per_acre~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
-              educ_none,data=base_no_fishing)
-summary(mod_2)
+mod_2_log <- lm(profit_per_acre_log~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
+              educ_bece+educ_voc +educ_O_level +educ_sss +educ_a_level +educ_t_cert +
+              educ_nurse +educ_tech +educ_bach +educ_master +educ_phd +educ_other,data=base)
+summary(mod_2_log)
+
+mod_2b <- lm(profit_per_acre~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
+              educ_bece+educ_voc +educ_O_level +educ_sss +educ_a_level +educ_t_cert +
+              educ_nurse +educ_tech +educ_bach +educ_master +educ_phd +educ_other,data=base)
+summary(mod_2b)
+
+mod_2b_log <- lm(profit_per_acre_log~farm_size+agey+spouse_live_hh+sex_male+fishing+own_business+
+                  educ_advanced,data=base)
+summary(mod_2b_log)
 
 mod_3 <- lm(profit_per_acre~farm_size+spouse_live_hh+sex_male+fishing+
               educ_none,data=base)
@@ -155,6 +214,10 @@ base <- base %>% left_join(educ_info_2)
 mod_4 <- lm(profit_per_acre~farm_size+spouse_live_hh+sex_male+fishing+
               educ_none+do_math,data=base)
 summary(mod_4)
+
+mod_4_log <- lm(profit_per_acre_log~farm_size+spouse_live_hh+sex_male+fishing+
+              educ_advanced+do_math,data=base)
+summary(mod_4_log)
 
 ####### HOUSING INFO
 
@@ -184,6 +247,128 @@ mod_5 <- lm(profit_per_acre~light_eletricity+light_generator+cooking_full_gas+co
               wall_stone+wall_cement,data=base)
 summary(mod_5)
 
+mod_5_log <- lm(profit_per_acre_log~light_eletricity+light_generator+cooking_full_gas+cooking_full_eletricity+
+              toilet_flush+toilet_latrine+wall_mud+wall_wood+wall_iron+
+              wall_stone+wall_cement,data=base)
+summary(mod_5_log)
+
 mod_6 <- lm(profit_per_acre~light_eletricity+cooking_full_gas+
               toilet_flush+toilet_latrine+wall_mud+wall_wood+wall_iron,data=base)
 summary(mod_6)
+
+mod_6_log <- lm(profit_per_acre_log~light_eletricity+cooking_full_gas+
+              toilet_flush+toilet_latrine+wall_mud+wall_wood+wall_iron,data=base)
+summary(mod_6_log)
+
+work_force <- read_dta(here("raw_data/glss4","sec8c1.dta")) 
+
+work_force <- work_force %>% 
+    mutate(ID = paste(clust,nh,sep="_"),
+           harvest_sold_pre = s8cq6 == 1,
+           harvest_sold_gate = s8cq6 == 2,
+           harvest_sold_market = s8cq6 == 3,
+           harvest_sold_consumer = s8cq6 == 4,
+           harvest_sold_state_org = s8cq6 == 5,
+           harvest_sold_coop = s8cq6 == 6,
+           paid_at_sale = s8cq10a == 1,
+           paid_at_week = s8cq10a == 2,
+           paid_at_month = s8cq10a == 3,
+           paid_at_over_m = s8cq10a == 4,
+           males_on_farme = ifelse(s8cq17a>1e+100,0,s8cq17a),
+           females_on_farme = ifelse(s8cq17b>1e+100,0,s8cq17b),
+           workers_on_farm = males_on_farme + females_on_farme
+    ) %>% 
+  select(-clust,-nh,-starts_with("S8")) %>% 
+  group_by(ID) %>% 
+  summarise(harvest_sold_pre=any(harvest_sold_pre),
+            harvest_sold_gate=any(harvest_sold_gate),
+            harvest_sold_market=any(harvest_sold_market),
+            harvest_sold_consumer=any(harvest_sold_consumer),
+            harvest_sold_state_org=any(harvest_sold_state_org),
+            harvest_sold_coop=any(harvest_sold_coop),
+            paid_at_sale=any(paid_at_sale),
+            paid_at_week=any(paid_at_week),
+            paid_at_month=any(paid_at_month),
+            paid_at_over_m=any(paid_at_over_m),
+            males_on_farme=sum(males_on_farme),
+            females_on_farme=sum(females_on_farme),
+            workers_on_farm=sum(workers_on_farm))
+
+
+base <- base %>% left_join(work_force,by="ID")
+mod_7 <- lm(profit_per_acre~harvest_sold_gate+harvest_sold_market+
+              harvest_sold_consumer+harvest_sold_state_org+harvest_sold_coop+
+              paid_at_sale+paid_at_week+paid_at_month+males_on_farme+
+              females_on_farme+workers_on_farm,data=base)
+summary(mod_7)
+
+mod_7_log <- lm(profit_per_acre_log~harvest_sold_gate+harvest_sold_market+
+              harvest_sold_consumer+harvest_sold_state_org+harvest_sold_coop+
+              paid_at_sale+paid_at_week+paid_at_month+males_on_farme+
+              females_on_farme+workers_on_farm,data=base)
+summary(mod_7_log)
+
+mod_7b_log <- lm(profit_per_acre_log~harvest_sold_gate+harvest_sold_market+
+                  harvest_sold_consumer+harvest_sold_state_org+harvest_sold_coop+
+                  paid_at_sale+paid_at_week+paid_at_month+workers_on_farm,data=base)
+summary(mod_7b_log)
+
+
+
+###### Including Community Survey
+com_surv_2 <- read_dta(here("raw_data/glss4/community","cs2.dta")) 
+com_surv_2 <- com_surv_2 %>% 
+  mutate(clust = eanum + 4000,
+         most_impor_farming = s2q1a==1,
+         most_impor_fishing = s2q1a==2,
+         moto_road = s2q4 ==1,
+         moto_road_impassable = s2q6 ==1,
+         have_bar = s2q13 ==1,
+         have_post_of_pub_telephone = s2q15 == 1,
+         have_bank = s2q17 == 1,
+         have_daily_mkt = s2q19 ==1,
+         have_week_mkt = s2q21b ==1,
+         public_transp = s2q23 ==1,
+         people_come_for_job_farming = s2q32 == 1 & (s2q34a==1 |s2q34b==1)
+         
+  ) %>% select(-starts_with("s2"),-eanum,-region,-district)
+
+com_surv_2 <- com_surv_2 %>% group_by(clust) %>% mutate_if(is.logical,any)
+com_surv_2 <- com_surv_2 %>% unique()
+
+com_surv_4b <- read_dta(here("raw_data/glss4/community","cs4b.dta")) 
+com_surv_4b <- com_surv_4b %>% 
+  mutate(clust = eanum + 4000,
+         have_hospital = s4bq0 ==10 & s4bq5==1) %>% 
+  select(-starts_with("s4"),-eanum,-region,-district)
+com_surv_4b <- com_surv_4b %>% group_by(clust) %>% mutate_if(is.logical,any)
+com_surv_4b <- com_surv_4b %>% unique()
+
+com_surv_5b <- read_dta(here("raw_data/glss4/community","cs5b.dta")) 
+com_surv_5b <- com_surv_5b %>% 
+  mutate(clust = eanum + 4000,
+         have_agric_ext_center = s5bq5==1,
+         have_cooperative = s5bq10==1,
+         any_farm_use_fert = s5bq15 ==1,
+         any_farm_use_inset_herb = s5bq16 ==1,
+         any_farm_use_irrigate = s5bq17 ==1,
+         mutual_aid_farm = s5bq23==1
+  ) %>% 
+  select(-starts_with("s5"),-eanum,-region,-district)
+com_surv_5b <- com_surv_5b %>% group_by(clust) %>% mutate_if(is.logical,any)
+com_surv_5b <- com_surv_5b %>% unique()
+
+###### Join communit Survey
+
+base <- base %>% 
+  left_join(com_surv_2, by="clust") %>% 
+  left_join(com_surv_4b, by="clust") %>% 
+  left_join(com_surv_5b, by="clust")
+  
+mod_8_log <- lm(profit_per_acre_log~most_impor_farming + most_impor_fishing +  moto_road +
+                moto_road_impassable +  have_bar + have_post_of_pub_telephone + have_bank +
+                have_daily_mkt + have_week_mkt + public_transp + people_come_for_job_farming +
+                have_hospital + have_agric_ext_center + have_cooperative + any_farm_use_fert +
+                any_farm_use_inset_herb + any_farm_use_irrigate + mutual_aid_farm,data=base)
+summary(mod_8_log)
+
